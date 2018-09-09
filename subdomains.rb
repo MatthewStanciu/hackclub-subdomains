64c3c501f2c13ed8afb22d505a817df405ffbcc5
MatthewStanciu <mattbstanciu@gmail.com>
2018-09-09 11:59:39 -0400

    better parsing

--- subdomains.rb
+++ subdomains.rb
@@ -2,19 +2,13 @@ require 'octokit'
 require 'base64'
 require 'yaml'
 
-$client = Octokit::Client.new(:login => 'OrpheusUser', :password => 'OrpheusPass')
+$client = Octokit::Client.new(:login => 'Orpheus', :password => 'OrpheusPass')
 contents = $client.contents("hackclub/dns", :path => "hackclub.com.yaml")
 $decoded_content = Base64.decode64(contents.content)
 
 def append_subdomain(subdomain, host)
-  new_content = $decoded_content + subdomain + ":\n  ttl: 1\n  type: CNAME\n  value: " + host + "."
-
-  val_file = File.exist?("vals.yml") ? YAML.load_file("vals.yml") : File.new("vals.yml", "w")
-  File.write("vals.yml", new_content)
-  parsed_content = YAML.load_file("vals.yml")
-  sorted = Hash[ parsed_content.sort_by { |key, val| key } ]
-  File.write("vals.yml", sorted.to_yaml)
-
+  new_content = YAML.load($decoded_content + subdomain + ":\n  ttl: 1\n  type: CNAME\n  value: " + host + ".")
+  sorted = Hash[ new_content.sort_by { |key, val| key } ]
   update_file_on_github(subdomain, sorted.to_yaml)
 end
 
@@ -24,4 +18,4 @@ def update_file_on_github(subdomain, content)
 end
 
 # usage: append_subdomain("requested-subdomain", "where-its-hosted")
-# append_subdomain("cegg2ge2e", "wlcat.club")
+# append_subdomain("kewthljh", "wlcat.club")
