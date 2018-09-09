2265171b9d0f07e975460c05f479dd4d45bc8053
MatthewStanciu <mattbstanciu@gmail.com>
2018-09-09 14:57:46 -0400

    dynamic message

--- subdomains.rb
+++ subdomains.rb
@@ -2,20 +2,28 @@ require 'octokit'
 require 'base64'
 require 'yaml'
 
-$client = Octokit::Client.new(:login => 'Orpheus', :password => 'OrpheusPass')
+$client = Octokit::Client.new(:login => 'Orpheus', :password => 'OrpheusPassword')
 contents = $client.contents("hackclub/dns", :path => "hackclub.com.yaml")
 $decoded_content = Base64.decode64(contents.content)
+$blob_sha = contents.sha
 
 def append_subdomain(subdomain, host)
+  message = ""
   new_content = YAML.load($decoded_content + subdomain + ":\n  ttl: 1\n  type: CNAME\n  value: " + host + ".")
   sorted = Hash[ new_content.sort_by { |key, val| key } ]
-  update_file_on_github(subdomain, sorted.to_yaml)
+
+  puts $decoded_content[subdomain]
+  if $decoded_content[subdomain] != nil
+    message = "update subdomain #{subdomain}"
+  else
+    message = "add subdomain #{subdomain}"
+  end
+  update_file_on_github(subdomain, sorted.to_yaml, message)
 end
 
-def update_file_on_github(subdomain, content)
-  blob_sha = "f40f8454ffdd0392f0610f8687a29a2c9cca2305"
-  $client.update_contents("hackclub/dns", "hackclub.com.yaml", "add subdomain #{subdomain}", blob_sha, content)
+def update_file_on_github(subdomain, content, message)
+  $client.update_contents("hackclub/dns", "hackclub.com.yaml", message, $blob_sha, content)
 end
 
 # usage: append_subdomain("requested-subdomain", "where-its-hosted")
-# append_subdomain("kewthljh", "wlcat.club")
+# append_subdomain("westlafayette", "wlcat.club")
