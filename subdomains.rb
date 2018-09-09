6013cd114a4b5aca274e05150801dacde8847715
MatthewStanciu <mattbstanciu@gmail.com>
2018-09-09 01:13:04 -0400

    add string appending
    ok going to bed now

--- subdomains.rb
+++ subdomains.rb
@@ -2,6 +2,14 @@ require 'octokit'
 require 'dotenv/load'
 require 'base64'
 
-client = Octokit::Client.new(:login => 'orpheus', :password => 'orpheus-pass')
+client = Octokit::Client.new(:login => 'MatthewStanciu', :password => '1c78cdff7b0397d528ef589357cafcfd20ad3d9c')
 contents = client.contents("hackclub/dns", :path => "hackclub.com.yaml")
-puts Base64.decode64(contents.content)
+$decoded_content = Base64.decode64(contents.content)
+
+def append_subdomain(subdomain, host)
+  new_content = $decoded_content + subdomain + ":\n  ttl: 1\n  type: CNAME\n  value: " + host + "."
+
+  puts new_content
+end
+
+append_subdomain("westlafayette", "https://wlcat.club")
